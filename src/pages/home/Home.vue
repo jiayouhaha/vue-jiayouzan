4d2fa591fd04d63997360849192c34b82ced6257
airbreak <jiangjianming_m@163.com>
2018-01-30 01:07:47 +0800

    add sass in components

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -10,15 +10,36 @@
             </swiper>
             <div class="swiper-pagination"></div>
         </div>
+        <div class="fast-link-container">
+            <a v-for="item in fastLinkArr" :href="item.link" class="item">
+                <div class="item-img">
+                    <img :src="item.img">
+                </div>
+                <span class="item-title">{{item.title}}</span>
+            </a>
+        </div>
+        <div class="top-gas-station">
+            <top-gas-station :stationInfo="stationData"></top-gas-station>
+        </div>
     </div>
 </template>
 
 <script>
-    import banner1 from 'assets/images/home/banner@1.jpg'
-    import banner2 from 'assets/images/home/banner@2.jpg'
+
+    /*图片*/
+    import banner1 from 'assets/images/home/banner@1.png'
+    import banner2 from 'assets/images/home/banner@2.png'
     import banner3 from 'assets/images/home/banner@3.jpeg'
 
+    import fast1 from 'assets/images/home/park.png'
+    import fast2 from 'assets/images/home/search.png'
+    import fast3 from 'assets/images/home/activity2.png'
+    import fast4 from 'assets/images/home/insurance.png'
+
     import {swiper,swiperSlide} from 'vue-awesome-swiper';
+
+    import topGasStation from '../../components/top-gas-station.vue';
+
     var bannerArr=[
         {
             display: 1,
@@ -42,13 +63,37 @@
             title: "哈哈哈3"
         }
     ];
+
+    var fastLinkArr=[
+        {
+            title: '停车',
+            link:'http://www.tnar.cn/park_weixin/toquery.do?a=ce9b41b20ef97fb9db829c4c1f0ae674&c=ce9b41b20ef97fb9db829c4c1f0ae674',
+            img:fast1
+        },
+        {
+            title: '违章代缴',
+            link:'http://www.tnar.cn/park_weixin/toquery.do?a=ce9b41b20ef97fb9db829c4c1f0ae674&c=ce9b41b20ef97fb9db829c4c1f0ae674',
+            img:fast2
+        },
+        {
+            title: '活动',
+            link:'http://www.tnar.cn/park_weixin/toquery.do?a=ce9b41b20ef97fb9db829c4c1f0ae674&c=ce9b41b20ef97fb9db829c4c1f0ae674',
+            img:fast3
+        },
+        {
+            title: '保险',
+            link:'http://www.tnar.cn/park_weixin/toquery.do?a=ce9b41b20ef97fb9db829c4c1f0ae674&c=ce9b41b20ef97fb9db829c4c1f0ae674',
+            img:fast4
+        }
+    ];
+
     export default {
         name: 'home',
         data: function () {
             return {
                 swiperOptions:{
                     notNextTick: true,
-                    autoplay: 2000,
+                    autoplay: true,
                     grabCursor : true,
                     setWrapperSize :true,
                     autoHeight: true,
@@ -58,12 +103,19 @@
                         el: '.swiper-pagination'
                     }
                 },
-                bannerArr:bannerArr
+                bannerArr:bannerArr,
+                fastLinkArr:fastLinkArr,
+                stationData:{
+                    name:'武汉测试加油站',
+                    location:'湖北省武汉市珞瑜路123号'
+                }
+
             }
         },
         components: {
             'swiper':swiper,
-            'swiper-slide':swiperSlide
+            'swiper-slide':swiperSlide,
+            'top-gas-station':topGasStation
         },
     }
 </script>
